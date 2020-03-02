09eddef8b6821e9d012e639b5699179ae13baabd
Artur Viader <djvatio@hotmail.com>
2020-03-02 19:58:46 +0100

    Cambiada linea

--- DAM_M09/Model/Event.swift
+++ DAM_M09/Model/Event.swift
@@ -18,7 +18,7 @@ class Event: Object{
     @objc dynamic private var numEtapas: Int = 0
     @objc dynamic private var km: Int = 0
 
-    //Este es otro comentario
+    //Hola Oscar soy tu compañero Artur
     
     convenience init(nombre:String, popularidad:Int, pais: String, numEtapas: Int, km: Int) {
         self.init()
