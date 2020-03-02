c244a548a0a23f31611b98947370afd175e50b73
oscar <59737408+OscarRD2@users.noreply.github.com>
2020-03-02 20:04:28 +0100

    Modificaccion de la misma linea con mi compañero

--- DAM_M09/Model/Event.swift
+++ DAM_M09/Model/Event.swift
@@ -18,7 +18,7 @@ class Event: Object{
     @objc dynamic private var numEtapas: Int = 0
     @objc dynamic private var km: Int = 0
 
-    //Este es otro comentario
+    //Hola Artur soy Oscar tu compañero
     
     convenience init(nombre:String, popularidad:Int, pais: String, numEtapas: Int, km: Int) {
         self.init()
